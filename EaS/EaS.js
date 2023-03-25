f3ed2c4472919da747a210b549237ba3833afde5
16050 <16050@ecam.be>
2023-03-25 14:17:59 +0100

    First commit

---
+++ EaS/EaS.js
@@ -0,0 +1,15 @@
+const document = 'EaS.html'
+
+const container = document.querySelector('#container');
+// selects the #container div (don't worry about the syntax, we'll get there)
+
+console.dir(container.firstElementChild);                      
+// selects the first child of #container => .display
+
+const controls = document.querySelector('.controls');   
+// selects the .controls div
+
+console.dir(controls.previousElementSibling);                  
+// selects the prior sibling => .display
+
+const div = document.createElement('div');
